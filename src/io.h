ad483d291e5132df2e909d88166c99dc06b3f655
Amodh Dhakal <amodhakal@gmail.com>
2026-08-04 23:14:28 -0400

    feat: reset project

--- src/io.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-
-namespace IO {
-const std::string getFullFileContents(const char *filePath);
-}
--- tests/test_main.cpp
+++
@@ -1,5 +0,0 @@
-// Entry point for the doctest-based unit test suite.
-// Only this translation unit defines DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN;
-// every other test file simply includes the doctest header.
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest/doctest.h"
